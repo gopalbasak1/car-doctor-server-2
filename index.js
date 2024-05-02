61d815129719104d39342121c9c272b54ce1ae1a
gopalbasak1 <gopalbasak1223@gmail.com>
2024-05-02 23:39:00 +0600

    added checkout get post updated deleted add to the mongodb and connected to the client site

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const cors = require('cors');
 const app = express();
@@ -12,7 +12,6 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qhiqbma.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
-console.log(uri);
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -28,9 +27,74 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
+    const serviceCollection = client.db('carDoctor').collection('services');
+
+    const checkoutCollection = client.db('carDoctor').collection('checkout')
 
 
+    app.get('/services', async(req, res)=>{
+      const cursor = serviceCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
 
+    app.get('/services/:id', async(req, res)=> {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+
+      const options ={
+        projection: {title: 1, price: 1, service_id: 1, img: 1}
+      }
+
+      const result = await serviceCollection.findOne(query, options);
+      res.send(result);
+    });
+
+   // checkout
+  //  app.get('/checkout', async(req, res) => {
+  //   const cursor = checkoutCollection.find();
+  //   const result = await cursor.toArray();
+  //   res.send(result);
+  //  })
+
+  //http://localhost:5000/checkout?email=biqasif@mailinator.com&sort=1
+   app.get('/checkout', async(req, res) =>{
+    console.log(req.query.email);
+    let query = {};
+    if(req.query?.email){
+      query = {email: req.query.email}
+    }
+    const result = await checkoutCollection.find(query).toArray()
+    res.send(result)
+   });
+
+    app.post('/checkout', async(req, res) => {
+      const checkout = req.body;
+      console.log(checkout);
+      const result = await checkoutCollection.insertOne(checkout);
+      res.send(result)
+    });
+
+    app.patch('/checkout/:id', async(req, res) => {
+      const id = req.params.id;
+      const filter = {_id: new ObjectId(id)}
+      const updatedBooking = req.body;
+      console.log(updatedBooking);
+      const updateDoc = {
+        $set: {
+          status: updatedBooking.status
+        },
+      };
+      const result = await checkoutCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    })
+
+    app.delete('/checkout/:id', async(req, res) =>{
+        const id = req.params.id;
+        const query = {_id: new ObjectId(id)};
+        const result = await checkoutCollection.deleteOne(query);
+        res.send(result);
+    })
 
 
 
@@ -39,7 +103,7 @@ async function run() {
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    //await client.close();
   }
 }
 run().catch(console.dir);
